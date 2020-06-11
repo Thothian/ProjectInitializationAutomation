d5ccf7da351ae3886f6c8a585a76c428da475d51
GrimLS <graysonhieb21@gmail.com>
2020-06-11 11:38:59 -0400

    Initial commit

--- .my_commands.sh
+++ .my_commands.sh
@@ -3,6 +3,7 @@
 function create() {
     python create.py $1
     source ~/ProjectInitializationAutomation/.env
+    echo $FILEPATH
     cd $FILEPATH$1
     git init
     git remote add origin git@github.com:$USERNAME/$1.git
